3646e5ab2b58e12b72d1a3397edad32d67b8184c
MisakaNo の 小破站 <122191366+Misaka-blog@users.noreply.github.com>
2023-03-24 22:04:08 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,20 +5,23 @@ VMESS_WSPATH=${VMESS_WSPATH:-'/vmess'}
 VLESS_WSPATH=${VLESS_WSPATH:-'/vless'}
 TROJAN_WSPATH=${TROJAN_WSPATH:-'/trojan'}
 SS_WSPATH=${SS_WSPATH:-'/shadowsocks'}
-
 URL=${HOSTNAME}-8080.csb.app
 
 sed -i "s#UUID#$UUID#g;s#VMESS_WSPATH#$VMESS_WSPATH#g;s#VLESS_WSPATH#$VLESS_WSPATH#g;s#TROJAN_WSPATH#${TROJAN_WSPATH}#g;s#SS_WSPATH#${SS_WSPATH}#g" /etc/xray/config.json
 sed -i "s#VMESS_WSPATH#$VMESS_WSPATH#g;s#VLESS_WSPATH#$VLESS_WSPATH#g;s#TROJAN_WSPATH#${TROJAN_WSPATH}#g;s#SS_WSPATH#${SS_WSPATH}#g" /etc/nginx/nginx.conf
 
-vmlink=vmess://$(echo -n "{\"v\":\"2\",\"ps\":\"codesandbox_vmess\",\"add\":\"$URL\",\"port\":\"443\",\"id\":\"$UUID\",\"aid\":\"0\",\"net\":\"ws\",\"type\":\"none\",\"host\":\"$URL\",\"path\":\"$VMESS_WSPATH?ed2048\",\"tls\":\"tls\"}" | base64 -w 0)
-vllink="vless://"$UUID"@"$URL":443?encryption=none&security=tls&type=ws&host="$URL"&path="$VLESS_WSPATH"?ed2048#codesandbox_vless"
+vmlink=vmess://$(echo -n "{\"v\":\"2\",\"ps\":\"repl\",\"add\":\"$URL\",\"port\":\"443\",\"id\":\"$UUID\",\"aid\":\"0\",\"net\":\"ws\",\"type\":\"none\",\"host\":\"$URL\",\"path\":\"$VMESS_WSPATH?ed=2048\",\"tls\":\"tls\"}" | base64 -w 0)
+vllink="vless://"$UUID"@"$URL":443?encryption=none&security=tls&type=ws&host="$URL"&path="$VLESS_WSPATH"?ed=2048#codesandbox_vless"
 trlink="trojan://"$UUID"@"$URL":443?security=tls&type=ws&host="$URL"&path="$TROJAN_WSPATH"?ed2048#codesandbox_trojan"
 
 rm -rf /usr/share/nginx/*
 wget https://gitlab.com/Misaka-blog/xray-paas/-/raw/main/mikutap.zip -O /usr/share/nginx/mikutap.zip
 unzip -o "/usr/share/nginx/mikutap.zip" -d /usr/share/nginx/html
 rm -f /usr/share/nginx/mikutap.zip
+
+qrencode -o /usr/share/nginx/html/M$UUID.png $vmlink
+qrencode -o /usr/share/nginx/html/L$UUID.png $vllink
+qrencode -o /usr/share/nginx/html/T$UUID.png $trlink
 
 cat > /usr/share/nginx/html/$UUID.html<<-EOF
 <html>
@@ -41,10 +44,16 @@ div {
 <body bgcolor="#FFFFFF" text="#000000">
 <div><font color="#009900"><b>VMESS协议链接：</b></font></div>
 <div>$vmlink</div>
+<div><font color="#009900"><b>VMESS协议二维码：</b></font></div>
+<div><img src="/M$UUID.png"></div>
 <div><font color="#009900"><b>VLESS协议链接：</b></font></div>
 <div>$vllink</div>
+<div><font color="#009900"><b>VLESS协议二维码：</b></font></div>
+<div><img src="/L$UUID.png"></div>
 <div><font color="#009900"><b>TROJAN协议链接：</b></font></div>
 <div>$trlink</div>
+<div><font color="#009900"><b>TROJAN协议二维码：</b></font></div>
+<div><img src="/T$UUID.png"></div>
 <div><font color="#009900"><b>SS协议明文：</b></font></div>
 <div>服务器地址：$URL</div>
 <div>端口：443</div>
